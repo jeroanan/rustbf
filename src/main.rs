3bc04fa4c8aeda5831016077676e2814bba348bc
Dave Wilson <davidwil@posteo.de>
2023-05-13 15:25:29 +0000

    rename machine_state to processor

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,14 @@ use std::collections::HashMap;
 
 mod bf_config;
 mod bf_instructions;
-mod machine_state;
+mod processor;
 mod memory;
 mod rom;
 
 fn main() {
 
 
-    type ValueFunction = fn(&mut machine_state::MachineState);
+    type ValueFunction = fn(&mut processor::MachineState);
 
     let mut char_map: HashMap<char, ValueFunction>= HashMap::new();
     char_map.insert(bf_instructions::PTR_INC, inc_ptr);
@@ -22,7 +22,7 @@ fn main() {
 
     let mut rom = rom::initialize_rom();
     let memory = memory::initialize_memory();
-    let mut machine_state = machine_state::initialize_machine(memory);
+    let mut machine_state = processor::initialize_machine(memory);
 
     rom.read_program_file();
 
@@ -37,31 +37,31 @@ fn main() {
     }
 }
 
-fn inc_ptr(state: &mut machine_state::MachineState) {
+fn inc_ptr(state: &mut processor::MachineState) {
     state.inc_mem_ptr();
 }
 
-fn dec_ptr(state: &mut machine_state::MachineState) {
+fn dec_ptr(state: &mut processor::MachineState) {
     state.dec_mem_ptr();
 } 
 
-fn inc_loc(state: &mut machine_state::MachineState) {
+fn inc_loc(state: &mut processor::MachineState) {
     state.inc_mem_loc_val();
 }
 
-fn dec_loc(state: &mut machine_state::MachineState) {
+fn dec_loc(state: &mut processor::MachineState) {
     state.dec_mem_loc_val();
 }
 
-fn loop_begin(state: &mut machine_state::MachineState) {
+fn loop_begin(state: &mut processor::MachineState) {
     state.loop_begin();
 }
 
-fn loop_end(state: &mut machine_state::MachineState) {
+fn loop_end(state: &mut processor::MachineState) {
     state.loop_end();
 }
 
-fn put_char(state: &mut machine_state::MachineState) {
+fn put_char(state: &mut processor::MachineState) {
     let c = state.get_char_to_display();
     print!("{}", c);
 }
