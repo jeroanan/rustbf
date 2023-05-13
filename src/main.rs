975133ca6368eee09f395b767972f8c73a666504
Dave Wilson <davidwil@posteo.de>
2023-05-13 09:05:21 +0000

    Run basic hello world program

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
 use std::collections::HashMap;
 use std::fs;
 
-type BfMemMap = [i32; 1000];
+type BfMemMap = [usize; 1000];
 
 struct MachineState {
     memory: BfMemMap,
     mem_ptr: usize,
-    program: String,
+    program: [char; 1001],
     program_ctr: usize,
-    loops: BfMemMap,
+    loops: [usize; 1000],
     loop_ptr: usize,
 }
 
@@ -18,63 +18,105 @@ fn main() {
     let mut machine_state = MachineState {
         memory: [0; 1000],
         mem_ptr: 0,
-        program: String::from(""),
+        program: ['\0'; 1001],
         program_ctr: 0,
         loops: [0; 1000],
         loop_ptr: 0,
     };
 
-    type ValueFunction = fn(&mut MachineState) -> ();
+    type ValueFunction = fn(&mut MachineState) -> bool;
 
     let mut char_map: HashMap<char, ValueFunction>= HashMap::new();
     char_map.insert('>', inc_ptr);
     char_map.insert('<', dec_ptr);
-    char_map.insert('+', |state| state.memory[state.mem_ptr]+=1);
-    char_map.insert('-', |state| state.memory[state.mem_ptr]-=1);
+    char_map.insert('+', inc_loc);
+    char_map.insert('-', dec_loc);
     char_map.insert('[', loop_begin);
+    char_map.insert(']', loop_end);
+    char_map.insert('.', put_char);
 
     let contents = fs::read_to_string(file_name)
         .expect("Could not read file");
 
-    let in_chars = contents
+    let in_str = contents
         .split("\n")
-        .collect::<Vec<&str>>()
-        .join("");
+        .collect::<Vec<&str>>();
 
-    machine_state.program = in_chars.clone();
+    let joined =  in_str.join("");
 
-    for c in in_chars.chars() {
-        if let Some(func) = char_map.get(&c) {
-            func(&mut machine_state);
+    let in_chars = joined.chars();
+
+    // copy first 1000 chars from in_chars to machine_state.program
+    for (i, c) in in_chars.enumerate() {
+        if i > 999 {
+            break;
         }
+        machine_state.program[i] = c;
     }
+    machine_state.program[1000] = '\0';
 
-    println!("{:?}", machine_state.mem_ptr)
-
+    while machine_state.program[machine_state.program_ctr] != '\0' {
+        let c = machine_state.program[machine_state.program_ctr];
+        if let Some(func) = char_map.get(&c) {
+            if func(&mut machine_state) {
+                machine_state.program_ctr+=1;
+            }
+        } 
+    }
 }
 
-fn inc_ptr(state: &mut MachineState) {
+fn inc_ptr(state: &mut MachineState) -> bool {
     if state.mem_ptr < 999 {
         state.mem_ptr+=1;
     } else {
         state.mem_ptr = 0;
     }
+
+    return true;
 }
 
-fn dec_ptr(state: &mut MachineState) {
+fn dec_ptr(state: &mut MachineState) -> bool {
     if state.mem_ptr > 0 {
         state.mem_ptr-=1;
     } else {
         state.mem_ptr = 1000;
     }
+
+    return true;
 } 
 
-fn loop_begin(state: &mut MachineState) {
-    state.loops[state.loop_ptr] = state.memory[state.mem_ptr+1];
-    state.loop_ptr+=1;
+fn inc_loc(state: &mut MachineState) -> bool {
+    state.memory[state.mem_ptr]+=1;
+    return true;
 }
 
-/* 
-fn loop_end(m: &mut BfMemMap, mr: &mut usize, l: &mut BfMemMap, lp: &mut usize) {
+fn dec_loc(state: &mut MachineState) -> bool {
+    state.memory[state.mem_ptr]-=1;
+    return true;
 }
-*/
+
+fn loop_begin(state: &mut MachineState) -> bool {
+    state.program_ctr+=1;
+    state.loops[state.loop_ptr] = state.program_ctr;
+    state.loop_ptr+=1;
+
+    return false;
+}
+
+fn loop_end(state: &mut MachineState) -> bool {
+    if state.memory[state.mem_ptr] > 0 {
+        state.program_ctr = state.loops[state.loop_ptr-1];
+        return false;
+    }
+    state.loop_ptr-=1;
+
+    return true;
+}
+
+fn put_char(state: &mut MachineState) -> bool {
+    if let Some(c) = char::from_u32(state.memory[state.mem_ptr] as u32) {
+        print!("{}", c);
+    }
+
+    return true;
+}
